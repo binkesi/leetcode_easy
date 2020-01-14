3d1fb7326dc80404b6d4681bf0e5b66eccb3fea7
binkesi <sjtusgn@163.com>
2020-01-14 16:01:49 +0800

    add python hash method.

--- python/num1_twosum.py
+++ python/num1_twosum.py
@@ -1,6 +1,6 @@
 # https://leetcode-cn.com/problems/two-sum/
 class Solution:
-    def twoSum(self, nums: list, target: int) -> list:
+    def twoSum_sort(self, nums: list, target: int) -> list:
         tmp = list(nums)
         new_nums = sorted(nums)
         i = 0
@@ -18,8 +18,18 @@ class Solution:
                 j -= 1
         return "No sum match."
 
+    def twoSum_hash(self, nums: list, target: int) -> list:
+        hashmap = {}
+        for ind, num in enumerate(nums):
+            hashmap[num] = ind
+        for i, num in enumerate(nums):
+            j = hashmap.get(target-num)
+            if j is not None and i != j:
+                return [i, j]
+
 
 if __name__ == "__main__":
     num1 = [5, 7, 4, 3, 1, 10, 6]
     solution = Solution()
-    print(solution.twoSum(num1, 10))
+    print(solution.twoSum_sort(num1, 10))
+    print(solution.twoSum_hash(num1, 10))
