3dc38ebd7db8b85715cc68296db650df83ca569a
binkesi <sjtusgn@163.com>
2020-01-25 18:20:40 +0800

    solve n58 with python

---
+++ python/n58_LastLength.py
@@ -0,0 +1,18 @@
+# https://leetcode-cn.com/problems/length-of-last-word/
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        s = s.rstrip()
+        print(s)
+        print(s[-1])
+        if s is None:
+            return 0
+        for i in s[::-1]:
+            if i == ' ':
+                return (-1 - s[::-1].index(i))
+        return len(s)
+        
+        
+if __name__ == "__main__":
+    solu = Solution()
+    s = "a "
+    print(solu.lengthOfLastWord(s))
