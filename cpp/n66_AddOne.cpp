e1dd138dd641f4eac95e42ab49bed0e6d7ea0f51
ecnusgn <sjtusgn@163.com>
2020-01-27 21:06:41 +0800

    solve n66 with cpp

---
+++ cpp/n66_AddOne.cpp
@@ -0,0 +1,25 @@
+// https://leetcode-cn.com/problems/plus-one/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i = digits.size()-1; i >= 0; --i){
+            if(digits[i] == 9)digits[i] = 0;
+            else{
+                digits[i]++;
+                return digits;
+            }
+        }
+        digits.push_back(0);
+        digits[0] = 1;
+        return digits;
+    }
+};
+
+int main(){
+    vector<int> digits = {4, 3, 2, 3};
+    Solution solu;
+    cout << solu.plusOne(digits) << endl;
+}
