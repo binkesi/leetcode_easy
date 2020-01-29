3cd947def718aedc3f6457724e0eee2c17ddfa01
ecnusgn <sjtusgn@163.com>
2020-01-29 11:26:51 +0800

    another method of n88 with cpp

--- cpp/n88_MergeTwo.cpp
+++ cpp/n88_MergeTwo.cpp
@@ -1,10 +1,19 @@
 // https://leetcode-cn.com/problems/merge-sorted-array/
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n){
+        for(int i = m; i < m+n; ++i){
+            nums1[i] = nums2[i-m];
+        }
+        sort(nums1.begin(), nums1.end());
+    }
+
+
+    void merge_a(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int p1 = m - 1;
         int p2 = n - 1;
         int p = m + n - 1;
