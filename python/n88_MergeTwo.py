af96b4f932eb3930432d27ac4456b1bf98cbf2b0
ecnusgn <sjtusgn@163.com>
2020-01-29 10:54:34 +0800

    another method of python

--- python/n88_MergeTwo.py
+++ python/n88_MergeTwo.py
@@ -4,6 +4,25 @@ class Solution:
         for i in range(m, m+n):
             nums1[i] = nums2[i-m]
         nums1.sort()        
+
+    def merge_a(self, nums1, m: int, nums2, n: int) -> None:
+        p1 = m - 1
+        p2 = n - 1
+        p = m + n - 1
+        while(nums1[p] == 0 and p2 >=0):
+            if p1 < 0:
+                for i in range(0, p2+1):
+                    nums1[i] = nums2[i]
+                break
+            if nums2[p2] >= nums1[p1]:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            else:
+                nums1[p] = nums1[p1]
+                nums1[p1] = 0
+                p1 -= 1
+            p -= 1 
+            
         
         
 if __name__ == "__main__":
@@ -12,5 +31,5 @@ if __name__ == "__main__":
     nums2 = [2, 5, 6]
     m = 3
     n = 3
-    solu.merge(nums1, m, nums2, n)
+    solu.merge_a(nums1, m, nums2, n)
     print(nums1)
