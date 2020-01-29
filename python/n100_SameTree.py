ede929fe56695cee4977908e59748d412676ea52
ecnusgn <sjtusgn@163.com>
2020-01-29 11:36:37 +0800

    solve n100 with python

---
+++ python/n100_SameTree.py
@@ -0,0 +1,14 @@
+# https://leetcode-cn.com/problems/same-tree/
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p is None or q is None:
+            return (p is None and q is None)
+        if p.val != q.val:
+            return False
+        return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))  
