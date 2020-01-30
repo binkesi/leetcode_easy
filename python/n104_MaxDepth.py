d4d3a794a7bcd18fd066caaaaa87d9b1210854b1
ecnusgn <sjtusgn@163.com>
2020-01-30 15:48:42 +0800

    solve n104 with python

---
+++ python/n104_MaxDepth.py
@@ -0,0 +1,16 @@
+# https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+        
+        
+if __name__ == "__main__":
+    pass
