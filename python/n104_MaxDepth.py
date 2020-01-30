326c2fc5e36a2680171631a91b5077f1e7d032a6
ecnusgn <sjtusgn@163.com>
2020-01-30 16:01:59 +0800

    iterate method for n104 with python

--- python/n104_MaxDepth.py
+++ python/n104_MaxDepth.py
@@ -10,7 +10,20 @@ class Solution:
         if root is None:
             return 0
         return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
-        
+    
+    def maxDepth_a(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        depth = 0
+        stack = []
+        stack.append((1, root))
+        while stack != []:
+            current_depth, root = stack.pop()
+            if root is not None:
+                depth = max(depth, current_depth)
+                stack.append((current_depth+1, root.left))
+                stack.append((current_depth+1, root.right))
+        return depth
         
 if __name__ == "__main__":
     pass
