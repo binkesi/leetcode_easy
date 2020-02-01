4579b211b2f86995b1ac3f647d9fe8d1c2ebe042
ecnusgn <sjtusgn@163.com>
2020-02-01 11:45:41 +0800

    bfs method for n111 with cpp

--- cpp/n111_MinDeepth.cpp
+++ cpp/n111_MinDeepth.cpp
@@ -19,6 +19,26 @@ public:
         if (root -> right == NULL) return minDepth(root -> left) + 1;
         return min(minDepth(root -> left), minDepth(root -> right)) + 1;
     }
+    
+    int bfs_minDepth(TreeNode* root){
+        if (root == NULL) return 0;
+        int min_deep = 0;
+        vector<TreeNode*> bfs_queue = {};
+        bfs_queue.push_back(root);
+        while (!bfs_queue.empty()){
+            vector<TreeNode*> next_queue = {};
+            min_deep += 1;
+            while (!bfs_queue.empty()){
+                TreeNode* cur_node = bfs_queue.front();
+                bfs_queue.erase(bfs_queue.begin());
+                if (cur_node -> left == NULL && cur_node -> right == NULL) return min_deep;
+                if (cur_node -> left != NULL) next_queue.push_back(cur_node -> left);
+                if (cur_node -> right != NULL) next_queue.push_back(cur_node -> right);
+            }
+            bfs_queue = next_queue;
+        }
+        return 0;
+    }
 };
 
 int main(){
