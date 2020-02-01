5582b30dca53c635d07941fe3c28064bfded83f4
ecnusgn <sjtusgn@163.com>
2020-02-01 11:21:46 +0800

    BFS method for n111 with python

--- python/n111_MinDeepth.py
+++ python/n111_MinDeepth.py
@@ -11,7 +11,22 @@ class Solution:
         if root.left is None: return self.minDepth(root.right) + 1
         if root.right is None: return self.minDepth(root.left) + 1
         return min(self.minDepth(root.left), self.minDepth(root.right)) + 1  
-        
+    
+    def minDepth_bfs(self, root: TreeNode) -> int:
+        if root is None: return 0
+        min_deep = 0
+        bfs_queue = []
+        bfs_queue.append(root)
+        while bfs_queue != []:
+            next_queue = []
+            min_deep += 1
+            while bfs_queue != []:
+                cur_node = bfs_queue.pop(0)
+                if cur_node.left is None and cur_node.right is None: return min_deep
+                if cur_node.left is not None: next_queue.append(cur_node.left)
+                if cur_node.right is not None: next_queue.append(cur_node.right)
+            bfs_queue = next_queue
+            
         
 if __name__ == "__main__":
     pass
