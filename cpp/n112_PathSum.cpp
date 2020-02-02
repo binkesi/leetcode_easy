77e7e19adb4a1c1964a916d1381b563786133557
ecnusgn <sjtusgn@163.com>
2020-02-02 10:56:22 +0800

    dfs method for n112 with cpp

--- cpp/n112_PathSum.cpp
+++ cpp/n112_PathSum.cpp
@@ -1,6 +1,7 @@
 // https://leetcode-cn.com/problems/path-sum/
 #include <iostream>
 #include <vector>
+#include <stack>
 using namespace std;
 
 struct TreeNode {
@@ -16,6 +17,26 @@ public:
         if (root == NULL) return false;
         if (root -> left == NULL && root -> right == NULL) return sum == root -> val;
         return hasPathSum(root -> left, sum - (root -> val)) || hasPathSum(root -> right, sum - (root -> val));
+    }
+    
+    bool dfs_hasPathSum(TreeNode* root, int sum) {
+        if (root == NULL) return false;
+        int path_sum = 0;
+        stack <pair<TreeNode*, int>> node_stack;
+        TreeNode* p = root;
+        while (!node_stack.empty() || p != NULL){
+            while (p != NULL){
+                path_sum += p -> val;
+                node_stack.push(pair<TreeNode*, int>(p, path_sum));
+                p = p -> left;
+            }
+            p = node_stack.top().first;
+            path_sum = node_stack.top().second;
+            node_stack.pop();            
+            if (p -> left == NULL && p -> right == NULL && path_sum == sum) return true;          
+            p = p -> right;
+        }
+        return false;
     }
 };
 
