09be64c102cd69af42c95a037747e5ecaf6089f7
ecnusgn <sjtusgn@163.com>
2020-02-09 12:16:53 +0800

    solve n155 with python

---
+++ python/n155_minStack.py
@@ -0,0 +1,43 @@
+# https://leetcode-cn.com/problems/min-stack/
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        
+
+    def push(self, x: int) -> None:
+        if self.stack == []:
+            self.stack.append((x, x))
+        else:
+            if x < self.getMin():
+                self.stack.append((x, x))
+            else:
+                self.stack.append((x, self.getMin()))
+
+    def pop(self) -> None:
+        if self.stack == []:
+            return
+        else:
+            self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+        
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+
+if __name__ == "__main__":
+    pass
