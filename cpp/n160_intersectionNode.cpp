eb9dccb7488ecaa5f75a46e958b73735fd7349a6
ecnusgn <sjtusgn@163.com>
2020-02-11 17:58:53 +0800

    solve n160 with cpp

---
+++ cpp/n160_intersectionNode.cpp
@@ -0,0 +1,30 @@
+// https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+// Definition for singly-linked list.
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* p_a = headA;
+        ListNode* p_b = headB;
+        while (p_a != p_b) {
+            if (p_a == NULL) p_a = headB;
+            else p_a = p_a -> next;
+            if (p_b == NULL) p_b = headA;
+            else p_b = p_b -> next;
+        }
+        return p_a;
+    }
+};
+
+int main(){
+    return 0;
+}
