ebd67f871fb74d8b3c69a26cf1de452de37aa48d
ecnusgn <sjtusgn@163.com>
2020-02-18 20:37:57 +0800

    edit n202 with cpp

--- cpp/n202_HappyNumber.cpp
+++ cpp/n202_HappyNumber.cpp
@@ -5,33 +5,28 @@
 #include <math.h>
 using namespace std;
 
-class Solution
-{
+class Solution{
 public:
-    bool HappyNumber(int num)
-    {
+    int cal_num(int num){
+        int res = 0;
+        while (num > 0){
+            res += pow((num % 10), 2);
+            num = num / 10;
+        }
+        return res;
+    }
+    
+    bool HappyNumber(int num){
         int slow = cal_num(num);
         int fast = cal_num(num);
         fast = cal_num(fast);
-        while (slow != fast) 
-        {
+        while (slow != fast){
             slow = cal_num(slow);
             fast = cal_num(fast);
             fast = cal_num(fast);
         }
         if (slow == 1) return true;
         else return false;
-    }
-    
-    int cal_num(int num)
-    {
-        int res = 0;
-        while (num > 0) 
-        {
-            res += pow((num % 10), 2);
-            num = num / 10;
-        }
-        return res;
     }
 };
 
