ac82871b58c17604addb18a37faf368e45165851
binkesi <sjtusgn@163.com>
2020-02-18 15:43:56 +0800

    solve n198 with python

---
+++ python/n198_StealMoney.py
@@ -0,0 +1,20 @@
+# https://leetcode-cn.com/problems/house-robber/
+
+class Solution:
+    def rob(self, nums) -> int:
+        if len(nums) == 0: return 0
+        if len(nums) == 1: return nums[0]
+        if len(nums) == 2: return max(nums[0], nums[1])
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = nums[1]
+        dp[2] = nums[0] + nums[2]
+        for i in range(3, len(nums)):
+            dp[i] = max(dp[i-3], dp[i-2]) + nums[i]
+        return max(dp[len(nums)-2], dp[len(nums)-1]) 
+
+
+if __name__ == "__main__":
+    nums = [2,7,9,3,1]
+    solu = Solution()
+    print(solu.rob(nums))
