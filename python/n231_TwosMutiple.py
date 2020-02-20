749f728d8c8e8e1e70d48c377facd6f9b7dd978c
ecnusgn <sjtusgn@163.com>
2020-02-20 20:48:53 +0800

    solve n231 using & with python

--- python/n231_TwosMutiple.py
+++ python/n231_TwosMutiple.py
@@ -8,8 +8,13 @@ class Solution:
                 return False
             n = n // 2
         return True
+        
+    def isPowerOfTwo_a(self, n: int) -> bool:
+        if n <= 0: return False
+        return n == n & (-n)
 
 
 if __name__ == "__main__":
     solu = Solution()
     print(solu.isPowerOfTwo(64))
+    print(solu.isPowerOfTwo_a(64))
