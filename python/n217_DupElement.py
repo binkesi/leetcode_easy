02028d7d4767e932d15db5a9fe1bb6c16bf8a5a1
binkesi <sjtusgn@163.com>
2020-02-20 14:39:58 +0800

    another method for n217 with python

--- python/n217_DupElement.py
+++ python/n217_DupElement.py
@@ -6,8 +6,12 @@ class Solution:
             num_dic[j] = i
         return len(num_dic.keys()) != len(nums)
         
+    def containsDuplicate_a(self, nums) -> bool:
+        return len(set(nums)) != len(nums)
+        
         
 if __name__ == "__main__":
     solu = Solution()
     nums = [1, 2, 4, 6, 5, 4]
     print(solu.containsDuplicate(nums))
+    print(solu.containsDuplicate_a(nums))
