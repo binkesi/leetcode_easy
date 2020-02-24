3335dd0a9f37c386b2af69013c2c7fc048428e58
binkesi <sjtusgn@163.com>
2020-02-24 15:27:43 +0800

    solve n263 with cpp

--- cpp/n263_isUgly.cpp
+++ cpp/n263_isUgly.cpp
@@ -7,6 +7,15 @@ using namespace std;
 class Solution{
 public:
     bool isUgly(int num){
+        if (num <= 0) return false;
         while (num % 2 == 0) num /= 2;
+        while (num % 3 == 0) num /= 3;
+        while (num % 5 == 0) num /= 5;
+        return num == 1;
     }
+};
+
+int main(){
+    Solution solu;
+    cout << solu.isUgly(24) << endl;
 }
