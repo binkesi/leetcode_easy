e47bbe5225c6273f061748efccdaa77fcbe158fb
binkesi <sjtusgn@163.com>
2020-02-24 14:12:45 +0800

    update n263 with python

--- python/n263_isUgly.py
+++ python/n263_isUgly.py
@@ -2,19 +2,19 @@
 class Solution:
     def isUgly(self, num: int) -> bool:
         if num <= 0: return False
-        while num > 1:
-            if num % 2 == 0:
-                num = num / 2
-                continue
-            if num % 3 == 0:
-                num = num / 3
-                continue
-            if num % 5 == 0:
-                num = num / 5
-                continue
-            else:
-                return False
-        return True
+        while num % 2 == 0:
+            num = num / 2
+            continue
+        while num % 3 == 0:
+            num = num / 3
+            continue
+        while num % 5 == 0:
+            num = num / 5
+            continue
+        if num == 1:
+            return True
+        else:
+            return False
 
 
 if __name__ == "__main__":
