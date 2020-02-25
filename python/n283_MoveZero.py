61f035facb460c18b475e6762affaa958706ed0f
binkesi <sjtusgn@163.com>
2020-02-25 10:41:53 +0800

    easy method for n283 with python

--- python/n283_MoveZero.py
+++ python/n283_MoveZero.py
@@ -12,6 +12,17 @@ class Solution:
             while i > 0 and nums[i] != 0 and nums[i-1] == 0:
                 swap(i, i-1, nums)
                 i -= 1
+                
+    def moveZeroes_a(self, nums) -> None:
+        def swap(i, j, nums):
+            tmp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = tmp  
+        last_not_zero = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                swap(last_not_zero, i, nums)
+                last_not_zero += 1
 
 
 if __name__ == "__main__":
