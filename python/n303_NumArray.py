0ca4692889f26a343a4d9ecd0196c4b0327b66da
binkesi <sjtusgn@163.com>
2020-02-26 16:12:03 +0800

    use dp for n202 with python

--- python/n303_NumArray.py
+++ python/n303_NumArray.py
@@ -3,12 +3,17 @@ class NumArray:
 
     def __init__(self, nums):
         self.nums = nums
+        if not self.nums:
+            return
+        self.dp = [0] * len(self.nums)
+        self.dp[0] = self.nums[0]
+        for k in range(1, len(self.nums)):
+            self.dp[k] = self.dp[k-1] + self.nums[k]        
 
     def sumRange(self, i: int, j: int) -> int:
-        sum = 0
-        for k in range(i, j+1):
-            sum += self.nums[k]
-        return sum       
+        if i == 0:
+            return self.dp[j]
+        else: return self.dp[j] - self.dp[i-1]
 
 
 # Your NumArray object will be instantiated and called as such:
