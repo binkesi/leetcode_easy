df9467b588a3f7d9d3be781a29b8d0935068c2cd
binkesi <sjtusgn@163.com>
2020-02-27 15:56:35 +0800

    convert string to list will parse more quickly

--- python/n345_ReverseVowels.py
+++ python/n345_ReverseVowels.py
@@ -17,10 +17,26 @@ class Solution:
             else:
                 head += 1
                 tail -= 1 
-        return s                
+        return s     
+
+    def reverseVowels_a(self, s: str) -> str:   
+        vowels = "aeiouAEIOU"
+        head = 0
+        tail = len(s) - 1
+        arr = list(s)
+        while head < tail:
+            while s[head] not in vowels and head < tail:
+                head += 1
+            while s[tail] not in vowels and head < tail:
+                tail -= 1
+            if head < tail:
+                arr[head], arr[tail] = arr[tail], arr[head]
+                head += 1
+                tail -= 1
+        return ''.join(arr)
             
             
 if __name__ == "__main__":
     solu = Solution()
     s = "leEtcOde"
-    print(solu.reverseVowels(s))
+    print(solu.reverseVowels_a(s))
