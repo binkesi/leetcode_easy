8d56dd4c6d24618d1f554b75c9dfc03ea4d61644
binkesi <sjtusgn@163.com>
2020-02-27 09:59:42 +0800

    solve n344 with python

---
+++ python/n344_ReverseString.py
@@ -0,0 +1,22 @@
+# https://leetcode-cn.com/problems/reverse-string/
+
+class Solution:
+    def reverseString(self, s) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        head = 0
+        tail = len(s)-1
+        while head < tail:
+            tmp = s[head]
+            s[head] = s[tail]
+            s[tail] = tmp
+            head += 1
+            tail -= 1
+
+
+if __name__ == "__main__":
+    solu = Solution()
+    s = ["h","e","l","l","o"]
+    solu.reverseString(s)
+    print(s)
