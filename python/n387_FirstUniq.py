9316836403fdb4954c34d2a5562580cb038bb76b
binkesi <sjtusgn@163.com>
2020-03-02 15:28:48 +0800

    solve n387 with python

---
+++ python/n387_FirstUniq.py
@@ -0,0 +1,19 @@
+# https://leetcode-cn.com/problems/first-unique-character-in-a-string/
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        index_list = []
+        for i in range(len(s)):
+            if i == s.index(s[i]):
+                index_list.append(i)
+            elif s.index(s[i]) in index_list:
+                index_list.remove(s.index(s[i]))
+        if not index_list:
+            return -1
+        else:
+            return index_list[0]
+            
+            
+if __name__ == "__main__":
+    solu = Solution()
+    s = "loveleetcode"
+    print(solu.firstUniqChar(s))
