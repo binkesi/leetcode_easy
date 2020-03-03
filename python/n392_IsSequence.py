8ce1a366baf07cf59fa7e5669f58c20ee4d00ac7
binkesi <sjtusgn@163.com>
2020-03-03 17:24:38 +0800

    update n392 with python

--- python/n392_IsSequence.py
+++ python/n392_IsSequence.py
@@ -3,8 +3,22 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         i = j = 0
-        s1 = len(s) - 1
-        s2 = len(t) - 1
-        while i <=
+        s1 = len(s)
+        s2 = len(t)
+        if s2 < s1: return False
+        if s1 == 0: return True
+        while i < s1:
+            if j >= s2: return False
+            while j < s2 and t[j] != s[i]:
+                j += 1
+                if j >= s2:
+                    return False
+            i += 1
+            j += 1
+        return True 
+        
+
+if __name__ == "__main__":
+    pass
             
         
