bf073bcc6bc1cf9a8bc60416469c8524237db969
binkesi <sjtusgn@163.com>
2020-03-05 11:26:55 +0800

    solve n405 with python

--- python/n405_ToHex.py
+++ python/n405_ToHex.py
@@ -2,6 +2,16 @@
 
 class Solution:
     def toHex(self, num: int) -> str:
-        hex_lis = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
-        if num < 0:
-            num = ~num
+        hex_str = "0123456789abcdef"
+        num &= 0xffffffff
+        mask = 0b1111
+        res = ""
+        while num > 0:
+            res += hex_str[num & mask]
+            num >>= 4
+        return res[::-1] if res else "0"
+        
+        
+if __name__ == "__main__":
+    solu = Solution()
+    print(solu.toHex(-26))
