170e05d239a0b4cba6c42e2fc2db55f3a2422ff7
sungn2 <sungn2@lenovo.com>
2020-03-23 18:03:03 +0800

    add heappush and heappop

--- python_cookbook/heapq_test.py
+++ python_cookbook/heapq_test.py
@@ -3,4 +3,7 @@ import heapq
 if __name__ == "__main__":
     nums = [1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2]
     heapq.heapify(nums)
+    heapq.heappush(nums, -9)
+    print(nums)
+    print(heapq.heappop(nums))
     print(nums)
