1cc49517ae49a50826267d1a17677b71dcb7e104
sungn2 <sungn2@lenovo.com>
2020-03-24 17:10:27 +0800

    dict key, items are similar to set, has &,- oper

---
+++ python_cookbook/Set_oper.py
@@ -0,0 +1,19 @@
+if __name__ == "__main__":
+    a = {
+        'x' : 1,
+        'y' : 2,
+        'z' : 3
+    }
+    
+    b = {
+        'w' : 10,
+        'x' : 11,
+        'y' : 2
+    }
+    print(a.keys() & b.keys())
+    print(a.keys() - b.keys())    
+    # make new dict without several keys
+    c = {key:a[key] for key in a.keys() - {'z', 'w'}}
+    for key, value in c.items():
+        print((key, value))
+    
