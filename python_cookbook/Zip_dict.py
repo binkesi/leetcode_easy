161dd55e69d1a9e8d2e02728d1c2ec031bd39013
sungn2 <sungn2@lenovo.com>
2020-03-24 16:44:54 +0800

    play zip with dict

---
+++ python_cookbook/Zip_dict.py
@@ -0,0 +1,11 @@
+
+if __name__ == "__main__":
+    prices = {
+        'ACME': 45.23,
+        'AAPL': 612.78,
+        'IBM': 205.55,
+        'HPQ': 37.20,
+        'FB': 10.75
+    }
+    print(min(zip(prices.values(), prices.keys())))
+    print(min(prices, key=lambda k: prices[k]))
