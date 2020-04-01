e401c6f970c05a3fe3fe609ee951aa3b2c39b365
binkesi <sjtusgn@163.com>
2020-04-01 17:41:05 +0800

    solve n463 with cpp

--- cpp/n463_IslandRound.cpp
+++ cpp/n463_IslandRound.cpp
@@ -6,6 +6,22 @@ using namespace std;
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-
+        int count = 0;
+        for(int i = 0;i<grid.size();++i){
+            for( int j =0;j<grid[0].size();++j){ 
+                if(grid[i][j] == 1) ++count;
+            }
+        }
+        for(int i = 0;i<grid.size()-1;++i){
+            for( int j =0;j<grid[0].size()-1;++j){
+                if(grid[i][j] == 1 && grid[i][j+1] == 1 &&grid[i+1][j] == 1 && grid[i+1][j+1] == 1)
+                    --count;
+            }
+        }
+        return 2*count+2;
     }
 };
+
+int main(){
+    return 0;
+}
