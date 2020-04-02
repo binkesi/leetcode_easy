4fa2dc090f221c80a798f34f4b41c97892d69e8c
binkesi <sjtusgn@163.com>
2020-04-02 16:59:58 +0800

    n482 another python method

--- python/n482_FormatKey.py
+++ python/n482_FormatKey.py
@@ -11,9 +11,18 @@ class Solution:
         S = ''.join(lis)
         return S
         
+    def licenseKeyFormatting_a(self, S: str, K: int) -> str:       
+        S = S.replace("-", "")
+        S = S.upper()
+        l = len(S) - K
+        while l > 0:           
+            S = S[0:l] + "-" + S[l:]
+            l = l - K
+        return S    
+        
 
 if __name__ == "__main__":
     solu = Solution()
     s = "5F3Z-2e-9-w"
     k = 4
-    print(solu.licenseKeyFormatting(s, k))
+    print(solu.licenseKeyFormatting_a(s, k))
