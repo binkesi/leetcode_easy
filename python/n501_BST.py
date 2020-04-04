6b55b53084370015f4ba761bc388559ee60c0290
ecnusgn <sjtusgn@163.com>
2020-04-04 16:10:41 +0800

    solve n501 with python

--- python/n501_BST.py
+++ python/n501_BST.py
@@ -8,21 +8,32 @@ class TreeNode:
 
 class Solution:
     def findMode(self, root: TreeNode):
+        lis = []
+        if root is None: return lis
+        self.TransTree(root, lis)
         res = []
-        node_dict = {}
-        self.TransTree(root, node_dict)
-        sort()
-        
-    def TransTree(self, root: TreeNode, node_dict):
-        if root is None:
-            return
-        else:
-            if root.val not in node_dict.keys():
-                node_dict[root.val] = 1
+        max_num = 0
+        cur_num = 1
+        l = len(lis)
+        for i in range(1, l+1):
+            if i == l or lis[i] != lis[i-1]:
+                if cur_num > max_num:
+                    res.clear()
+                    res.append(lis[i-1])
+                    max_num = cur_num
+                elif cur_num == max_num:
+                    res.append(lis[i-1])
+                cur_num = 1
             else:
-                node_dict[root.val] += 1
-        self.TransTree(root.left, node_dict)
-        self.TransTree(root.right, node_dict)
+                cur_num += 1
+        return res                
+    
+    def TransTree(self, root: TreeNode, lis):
+        if root is None: return
+        self.TransTree(root.left, lis)
+        lis.append(root.val)
+        self.TransTree(root.right, lis)
+        
 
 
 if __name__ == "__main__":
