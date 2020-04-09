11c17ac075ad7f3c6c9b321b5ac0258a75d1ee96
binkesi <sjtusgn@163.com>
2020-04-09 18:06:20 +0800

    n530 with cpp

---
+++ cpp/n530_BSTMinus.cpp
@@ -0,0 +1,21 @@
+// https://leetcode-cn.com/problems/minimum-absolute-difference-in-bst/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int getMinimumDifference(TreeNode* root) {
+
+    }
+    
+    void TreeList(TreeNode* root, )
+};
