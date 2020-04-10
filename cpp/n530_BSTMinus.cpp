f950d58efd6e576b744bdb2c456f3594bf987a85
binkesi <sjtusgn@163.com>
2020-04-10 10:45:48 +0800

    solve n530 with cpp

--- cpp/n530_BSTMinus.cpp
+++ cpp/n530_BSTMinus.cpp
@@ -14,8 +14,23 @@ struct TreeNode {
 class Solution {
 public:
     int getMinimumDifference(TreeNode* root) {
-
+        vector<int> tree_val;
+        TreeList(root, tree_val);
+        if (tree_val.size() == 1) return 0;
+        int min_res = tree_val[1] - tree_val[0];
+        for (size_t i = 2; i < tree_val.size(); ++i)
+            min_res = min(min_res, tree_val[i] - tree_val[i-1]);
+        return min_res;
     }
     
-    void TreeList(TreeNode* root, )
+    void TreeList(TreeNode* root, vector<int>& tree_val){
+        if (root == NULL) return;
+        TreeList(root -> left, tree_val);
+        tree_val.push_back(root -> val);
+        TreeList(root -> right, tree_val);
+    }
 };
+
+int main(){
+    return 0;
+}
