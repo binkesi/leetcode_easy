91869b798e8420bdf802ca3f8a579ee18144a3f4
binkesi <sjtusgn@163.com>
2020-04-13 14:33:45 +0800

    solve n559 with python

---
+++ python/n559_NTreeDepth.py
@@ -0,0 +1,33 @@
+# https://leetcode-cn.com/problems/maximum-depth-of-n-ary-tree/
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+class Solution:
+    def __init__(self):
+        self.depth = 0
+        self.queue = []
+        
+    def maxDepth(self, root: 'Node') -> int:
+        if root is None: return 0
+        tmp_queue = []
+        tmp_queue.append(root)
+        self.queue.append(tmp_queue)
+        while len(self.queue[0]) != 0:
+            tmp_queue = []
+            for node in self.queue[0]:
+                if node is None or len(node.children) == 0:
+                    continue
+                for i in node.children:
+                    tmp_queue.append(i)
+            self.queue.append(tmp_queue)
+            self.queue.pop(0)
+            self.depth += 1
+        return self.depth
+        
+
+if __name__ == "__main__":
+    pass
+            
