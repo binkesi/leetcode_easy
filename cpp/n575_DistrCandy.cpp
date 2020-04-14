fb64392c65d46ff443b5460dc480e1f4168ac027
binkesi <sjtusgn@163.com>
2020-04-14 17:01:29 +0800

    solve n575 with cpp map

--- cpp/n575_DistrCandy.cpp
+++ cpp/n575_DistrCandy.cpp
@@ -14,6 +14,12 @@ public:
         for(auto i: candies) tmp.insert(i);
         return min(candies.size()/2, tmp.size());
     }
+    
+    int distributeCandies_a(vector<int>& candies) {
+        map <int, int> tmp;
+        for (auto i: candies) tmp.insert(pair<int, int>(i, 1));
+        return min(candies.size()/2, tmp.size());
+    }
 };
 
 int main(){
