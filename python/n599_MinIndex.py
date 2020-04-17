dcc2b7da443ac7af64859a247323aa326eb5e1e3
binkesi <sjtusgn@163.com>
2020-04-17 17:48:54 +0800

    solve n599 with python

---
+++ python/n599_MinIndex.py
@@ -0,0 +1,21 @@
+# https://leetcode-cn.com/problems/minimum-index-sum-of-two-lists/
+class Solution:
+    def findRestaurant(self, list1, list2):
+        relis = list(set(list1) & set(list2))
+        res = []
+        min_index = list1.index(relis[0]) + list2.index(relis[0])
+        res.append(relis[0])
+        for i in range(1, len(relis)):
+            this_index = list1.index(relis[i]) + list2.index(relis[i])
+            if this_index == min_index:
+                res.append(relis[i])
+            if this_index < min_index:
+                res.clear()
+                res.append(relis[i])
+                min_index = this_index
+        return res
+
+
+if __name__ == "__main__":
+    pass        
+        
