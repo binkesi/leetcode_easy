d6940a9ee1134af42dc497325039b2804ba8bd3e
binkesi <sjtusgn@163.com>
2020-04-22 15:32:22 +0800

    play 1-3 deque

--- python_cookbook/1-3deque.py
+++ python_cookbook/1-3deque.py
@@ -1,8 +1,18 @@
 from collections import deque
 
-def search(line):
-    
+def search(lines, pattern, history):
+    pre_lines = deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+            yield line, pre_lines
+        pre_lines.append(line)
+        
 
 if __name__ == "__main__":
-    with open(./test.txt) as f:
-        for line in f.read():
+    lines = deque(maxlen=5)
+    with open(r'D:\My_pycharm_project\leetcode_easy\python_cookbook\test.txt', "r") as f:
+        for line, pre_lines in search(f, "python", 5):
+            for pline in pre_lines:
+                print(pline, end='')
+            print(line, end='')
+            print('-' * 20)
