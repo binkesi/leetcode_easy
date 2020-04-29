cdb04db4f5b33ce58878e270c5a813579c0108af
binkesi <sjtusgn@163.com>
2020-04-29 09:54:32 +0800

    play 2-11 strip

---
+++ python_cookbook/2-11Strip.py
@@ -0,0 +1,9 @@
+import re
+
+text = "       ------------hello========="
+
+if __name__ == "__main__": 
+    aaa = text.strip()  
+    print(aaa)    
+    print(aaa.lstrip('-'))   
+    print(aaa.rstrip('='))
