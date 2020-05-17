ad63ed1587e470f4785f954d0df3c06725c67a93
ecnusgn <sjtusgn@163.com>
2020-05-17 16:13:13 +0800

    set venv

--- python_cookbook/11-1HttpHead.py
+++ python_cookbook/11-1HttpHead.py
@@ -1,12 +1,13 @@
 import requests
 
-resp = requests.head('http://www.python.org/index.html')
+resp = requests.head('http://www.baidu.com')
 
 status = resp.status_code
 last_modified = resp.headers['last-modified']
 content_type = resp.headers['content-type']
-content_length = resp.headers['content-length']
+#content_length = resp.headers['content-length']
 
 
 if __name__ == "__main__":
-    print(status, last_modified, content_type, content_length, sep=', ')
+    print(resp)
+    print(status, last_modified, content_type, sep=', ')
